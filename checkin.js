72fc25fd51746c99754bd5192ac3eeea028a8e23
shusheng00 <51360685+shusheng00@users.noreply.github.com>
2025-10-14 14:49:10 +0800

    Update checkin.js
    添加token

--- checkin.js
+++ checkin.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const CONFIG = {
-  token: '', // token
+  token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.e30.mXMRQogccGnt1dFUucoRmdQVqzK2P744J_wRgFEf-oE', // token
   checkinUrl: 'https://api-ic.yonyouauto.com/api/applet/hrempintegralsigninlist/querySignInNowPeriodSignIn?time=' + parseInt(new Date().getTime() / 1000), // 签到 URL
   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090c11)XWEB/11581'
 };
